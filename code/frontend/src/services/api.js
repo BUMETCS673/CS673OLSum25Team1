8591da89fcc12e0b127f411b65a637f2f7ea8c9e
Shaohua Yue <sajia23@bu.edu>
2025-06-15 15:07:06 -0400

    fix(frontend): update for token expired

--- code/frontend/src/services/api.js
+++ code/frontend/src/services/api.js
@@ -23,8 +23,10 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    if (error.response?.status === 401 || error.response?.status === 403) {
-      window.location.replace("/login");
+    if (error.response?.status === 401) {
+      if (window.location.pathname !== "/login") {
+        window.location.replace("/login");
+      }
     }
 
     return Promise.reject(error);
